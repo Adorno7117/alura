d4817a6d9a3d252f223920064d72be90909a80c3
Gabriel Adorno Simoso <ga7.adorno@outlook.com>
2025-02-17 19:20:41 -0300

    att

--- programação/nodeJs/JavaScript programação Orientada a Objetos/objetoLiteral.js
+++ programação/nodeJs/JavaScript programação Orientada a Objetos/objetoLiteral.js
@@ -1,25 +1,22 @@
-const user ={
-    nome: 'Juliana',
-    email: 'j@j.com',
-    nascimento: '2024-01-01',
-    role: 'estudante',
-    ativo: true,
-    exibirInfos: function(){
-        console.log(this.nome, this.email, this.nascimento, )
-    }
+const user = {
+  nome: "Juliana",
+  email: "j@j.com",
+  nascimento: "2024-01-01",
+  role: "estudante",
+  ativo: true,
+  exibirInfos: function () {
+    console.log(this.nome, this.email);
+  },
+};
+
+// user.exibirInfos()
+
+// const exibir = user.exibirInfos
+// exibir()
+
+const exibir = function() {
+  console.log(this.nome, this.email);
 }
 
-const admin = {
-    nome: "Mariana",
-    email: "m@m.com",
-    nascimento: "2024-01-01",
-    role: "admin",
-    ativo: true,
-    exibirInfos: function () {
-      console.log('curso criado');
-    },
-  };
-
-  Object.setPrototypeOf(admin, user)
-admin.criarCurso()
-admin.exibirInfos()
+const exibirNome = exibir.bind(user);
+exibirNome()
